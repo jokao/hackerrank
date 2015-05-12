5dd534fe28fd079ecdc684232f5b4788518a967c
jokao <joka.oyume@gmail.com>
2015-05-12 22:24:16 +0900

    First commit

---
+++ FunctionalProgramming/MissingNumbersFP.fsx
@@ -0,0 +1,25 @@
+﻿// https://www.hackerrank.com/challenges/missing-numbers-fp
+
+open System
+
+Console.In.ReadLine() |> ignore
+let a = Console.In.ReadLine().Split(' ') |> Array.map int
+Console.In.ReadLine() |> ignore
+let b = Console.In.ReadLine().Split(' ') |> Array.map int 
+
+
+let bMax = Array.max b
+let bMin = Array.min b
+
+let counts : int array = Array.zeroCreate (bMax - bMin + 1)
+
+for n in b do
+    counts.[n - bMin] <- counts.[n - bMin] + 1
+
+for n in a do
+    counts.[n - bMin] <- counts.[n - bMin] - 1
+
+for i in 0..counts.Length-1 do
+    if counts.[i] > 0 then printf "%d " (i + bMin)
+
+printfn ""
---
+++ FunctionalProgramming/Program.fs
@@ -0,0 +1,30 @@
+﻿// https://www.hackerrank.com/challenges/missing-numbers-fp
+
+open System
+
+let t = Console.In.ReadLine() |> int
+
+let rec gcd (x : int) (y : int) =
+    let max = Math.Max(x, y)
+    let min = Math.Min(x, y)
+
+    if (max % min = 0) then min
+    else gcd min (max % min)
+
+for i in 1..t do
+    let pair = Console.In.ReadLine().Split(' ')
+    let m = pair.[0] |> int
+    let l = pair.[1] |> int
+
+    let g = gcd m l
+
+    let top = g |> float |> Math.Sqrt |> Math.Floor |> int
+
+    let mutable counter = 0
+    for i in 1..top do
+        if g % i = 0 then counter <- counter + 1
+
+    counter <- counter * 2
+    if top * top = g then counter <- counter - 1
+
+    printfn "%d" counter
